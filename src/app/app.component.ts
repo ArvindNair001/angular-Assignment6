5a4a3f06c88909b1c301299bd4ade1749cb3a9f0
Arvind Hariharan Nair <arvdev96@gmail.com>
2019-05-05 21:24:03 +0530

    Assignment 6 Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Assignment6';
+export class AppComponent implements OnInit {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultPlan = 'basic';
+  user = {
+    email: '',
+    plan: '',
+  };
+
+  submitted_form = false;
+
+  ngOnInit() {
+    this.submitted_form = false;
+  }
+
+  onSubmit() {
+    this.submitted_form = true;
+    console.log(this.signUpForm.value);
+    this.user.email = this.signUpForm.value.email;
+    this.user.plan = this.signUpForm.value.subscriptions;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
